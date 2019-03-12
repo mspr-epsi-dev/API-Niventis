963f3fb40b647b6ccb329e6963e57a443bda32c9
maclovin <alexisbrunet34@gmail.com>
2019-03-12 17:23:51 +0100

    basic setup + test

---
+++ srve/init.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    "mongodbDev" : "mongodb://localhost/mspr-api", 
+    "mongodbProd": "mongodb://mongo:27017/mspr-api",
+
+};
---
+++ srve/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const database = require('./functions/databaseFunctions');
+
+const app =  express();
+const port = 3000
+
+database.connect();
+
+app.listen(port, () => {
+    
+    console.log( "serveur launched, listening on port " + port );
+    console.log("environment : " + app.settings.env);
+    
+
+})
